79dbbcc425cb0871b9565726350b3bfa4048c367
andreyscott <andreyscott>
2022-01-24 20:34:19 +0100

    A basic focus app created in react native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { Focus } from './features/focus/focus';
 
 export default function App() {
+const [focusSubject, setFocusSubject ] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Yeah </Text>
-      <StatusBar style="auto" />
+      
+      <Text> {focusSubject ? (<Text> is this True </Text>) : (  <Focus /> )}</Text>
     </View>
   );
 }
